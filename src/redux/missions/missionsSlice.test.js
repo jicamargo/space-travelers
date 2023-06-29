1118d77c45c8039fa17173a13ca60ac5a11a0c9a
jic <ji_camargo@yahoo.com>
2023-06-29 15:17:42 -0500

    add missionSlice.test.js

---
+++ src/redux/missions/missionsSlice.test.js
@@ -0,0 +1,18 @@
+import MissionsReducer, { returnAllMissions, GetMissions } from './missionsSlice';
+
+describe('MissionsSlice', () => {
+  test('should handle returnAllMissions pending', () => {
+    const nextState = MissionsReducer(undefined, returnAllMissions.pending());
+    expect(nextState.loading).toBe(true);
+  });
+
+  test('should handle returnAllMissions fulfilled', () => {
+    const missionsData = [{ mission_id: '1', name: 'Mission 1' }];
+    const nextState = MissionsReducer(
+      undefined,
+      returnAllMissions.fulfilled(missionsData)
+    );
+    expect(nextState.loading).toBe(false);
+    expect(nextState.missions).toEqual(missionsData);
+  });
+});
